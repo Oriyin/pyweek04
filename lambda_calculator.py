3674d80fd24a9a49d0ebba54fe70d33c208faf49
Oriyin <66011050@kmitl.ac.th>
2024-07-23 14:09:15 +0700

    Add files via upload

---
+++ lambda_calculator.py
@@ -0,0 +1,10 @@
+Add = lambda x, y: x+y
+Subtract = lambda x,y: x-y
+Multiplication = lambda x,y: x*y
+Division = lambda x,y: x/y
+x=10
+y=20
+print(Add(x,y))
+print(Subtract(x,y))
+print(Multiplication(x,y))
+print(Division(x,y))
