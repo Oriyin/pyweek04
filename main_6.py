9ff65a8336e9a1a8a91d2f3f16050a63a66eea15
Oriyin <66011050@kmitl.ac.th>
2024-07-23 14:18:15 +0700

    Add files via upload

---
+++ main_6.py
@@ -0,0 +1,7 @@
+import string_operations_6 as so
+sample_string = "hello World"
+print("Original:", sample_string)
+print("Reversed:", so.reverse_string(sample_string))
+print("Capitalized:", so.capitalize_string(sample_string))
+print("Lowercase:", so.lowercase_string(sample_string))
+print("Uppercase:", so.uppercase_string(sample_string))
