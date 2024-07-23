5001074b5f3f1d6e14cc2757d13cbb03fa35dced
Oriyin <66011050@kmitl.ac.th>
2024-07-23 14:08:41 +0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def Add(A,B) :
+    return A+B
+def Subtract(A,B) :
+    return A-B
+def Multiply(A,B) :
+    return A*B
+def Divide(A,B) :
+    return A/B
+A=10
+B=20
+print(Add(A,B))
+print(Subtract(A,B))
+print(Multiply(A,B))
+print(Divide(A,B))
