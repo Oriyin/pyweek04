b5c5163f0a651de7d26e18e5ef0690cf31155469
Oriyin <66011050@kmitl.ac.th>
2024-07-23 14:16:04 +0700

    Add files via upload

---
+++ 6string_operations.py
@@ -0,0 +1,11 @@
+def reverse_string(input):
+    return input[::-1]
+
+def capitalize_string(input):
+    return input.capitalize()
+
+def lowercase_string(input):
+    return input.lower()
+
+def uppercase_string(input):
+    return input.upper()
